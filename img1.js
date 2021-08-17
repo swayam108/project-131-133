5d4ed6f05326798cf0b31ccb75dabda53ef27548
swayam108 <79143263+swayam108@users.noreply.github.com>
2021-08-17 15:18:40 +0530

    Add files via upload

---
+++ img1.js
@@ -0,0 +1,44 @@
+img="";
+status="";
+object=[];
+function preload(){
+img=loadImage("img1.jpg");
+}
+function setup(){
+canvas=createCanvas(640,420);
+canvas.center();
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="Status-Detecting Object";
+}
+function draw(){
+
+image(img,0,0,640,420);
+if (status==true) {
+    for (i=0; i<object.lenght; i++) {
+        document.getElementById("status").innerHTML="Status-Object Detected";
+        document.getElementById("status2").innerHTML="coco ssd detect "+object.lenght+" object";
+        fill("#ff0800");
+        percent=floor(object[i].confidence*100)
+        text(object[i].label+" "+ percent+"%",object[i].x,object[i].y);
+        noFill();
+        stroke("#ff2600");
+        rect(object[i].x,object[i].y,object[i].width,object[i].height);
+    }
+}
+}
+function modelLoaded(){
+    console.log("model loaded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+if (error) {
+    console.log(error);
+} else {
+    console.log(results)
+    object=results;
+}
+}
+function back(){
+    window.location="index.html";
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function img1(){
+    window.location="img1.html";
+}
+function img2(){
+    window.location="img2.html";
+}
+function img3(){
+    window.location="img3.html";
+}
+function img4(){
+    window.location="img4.html";
+}
+function img5(){
+    window.location="img5.html";
+}
